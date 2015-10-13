969b1cb6a2b8d8f318cab1a9bf38b81a064844cd
DeveloperVix <vicomisterio@hotmail.com>
2015-10-12 20:42:35 -0500

    Clase Motor
    Clase Atributos
    Clase  Movimiento

---
+++ Motor/Motor_1.cpp
@@ -0,0 +1,156 @@
+#include <stdlib.h>
+#include <iostream>
+#include "movimiento.cpp"
+
+using namespace std;
+
+class Atributos{
+private:
+	int contador;
+	string nombre;
+	int posicion[2];//posicion x,y
+	int tamanio[2];//largo, ancho
+	int rotacion;
+	int color[4];//RGBA
+	string etiqueta;
+	bool precargaArte;
+	Movimiento g;
+
+public:
+
+	Atributos();
+
+	void setNombre(string);
+	string getNombre();
+	
+	void setPosicion(int,int);
+	int getPosicionX();
+	int getPosicionY();
+	
+	void setTamanio(int,int);
+	int getTamanioLargo();
+	int getTamanioAncho();
+	
+	void setRotacion(int);
+	int getRotacion();
+	
+	void setColor(int,int,int,int);
+	int getColorR();
+	int getColorG();
+	int getColorB();
+	int getColorA();
+	
+	void setEtiqueta(string);
+	string getEtiqueta();
+	
+	void setPrecargaArte(bool);
+	bool getPrecargaArte();
+	
+	void VerAtributos();
+};
+
+
+
+Atributos::Atributos(){
+	contador = 1;
+	nombre = "Actor ";
+	posicion[0] = posicion[1] = 0;
+	tamanio[0] = tamanio[1] = 100;
+	rotacion = 0;
+	color[0] = color[1] = color[2] = color[3] = 1;
+	etiqueta = "";
+	precargaArte = true;
+	
+	VerAtributos();
+}
+
+void Atributos::setNombre(string nom){
+	nombre = nom;
+}
+
+string Atributos::getNombre(){
+	return nombre;
+}
+
+void Atributos::setPosicion(int X, int Y){
+	posicion[0] = X;
+	posicion[1] = Y;
+}
+
+int Atributos::getPosicionX(){
+	return posicion[0];
+}
+
+int Atributos::getPosicionY(){
+	return posicion[1];
+}
+
+void Atributos::setTamanio(int W, int H){
+	tamanio[0] = W;
+	tamanio[1] = H;
+}
+
+int Atributos::getTamanioAncho(){
+	return tamanio[0];
+}
+
+int Atributos::getTamanioLargo(){
+	return tamanio[1];
+}
+
+void Atributos::setRotacion(int R){
+	rotacion = R;
+}
+
+int Atributos::getRotacion(){
+	return rotacion;
+}
+
+void Atributos::setColor(int R, int G, int B, int A){
+	color[0] = R;
+	color[1] = G;
+	color[2] = B;
+	color[3] = A;
+}
+
+int Atributos::getColorR(){
+	return color[0];
+}
+
+int Atributos::getColorG(){
+	return color[1];
+}
+
+int Atributos::getColorB(){
+	return color[2];
+}
+
+int Atributos::getColorA(){
+	return color[3];
+}
+
+void Atributos::setEtiqueta(string E){
+	etiqueta = E;
+}
+
+string Atributos::getEtiqueta(){
+	return etiqueta;
+}
+
+void Atributos::setPrecargaArte(bool PA){
+	precargaArte = PA;
+}
+
+bool Atributos::getPrecargaArte(){
+	return precargaArte;
+}
+
+void Atributos::VerAtributos(){
+	//imprimir todos los atributos
+}
+
+int main(){
+	Atributos actorParte_1;
+	Movimiento actorParte_2;
+	return 0;
+}
---
+++ Motor/movimiento.cpp
@@ -0,0 +1,87 @@
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+class Movimiento{
+private:
+	int velocidadLineal [2];//velocidad x,y
+	int velocidadAngular;
+	int velocidadMaxima;
+	bool aplicaMaxVelocidad;
+
+public:
+	
+	Movimiento();
+	
+	void setVelLineal(int,int);
+	int getVelLinealX();
+	int getVelLinealY();
+	
+	void setVelAngular(int);
+	int getVelAngular();
+	
+	void setVelMaxima(int);
+	int getVelMaxima();
+	
+	void setAplicaMaxVel(bool);
+	bool getAplicaMaxVel();
+	
+	void VerAtributos();
+	
+};
+
+Movimiento::Movimiento(){
+	velocidadLineal[0] = velocidadLineal[1] = 0;
+	velocidadAngular = 0;
+	velocidadMaxima = 0;
+	aplicaMaxVelocidad = false;
+	
+	VerAtributos();
+}
+
+void Movimiento::setVelLineal(int X, int Y){
+	velocidadLineal[0] = X;
+	velocidadLineal[1] = Y;
+}
+
+int Movimiento::getVelLinealX(){
+	return velocidadLineal[0];
+}
+
+int Movimiento::getVelLinealY(){
+	return velocidadLineal[1];
+}
+
+void Movimiento::setVelAngular(int velA){
+	velocidadAngular = velA;
+}
+
+int Movimiento::getVelAngular(){
+	return velocidadAngular;
+}
+
+void Movimiento::setVelMaxima(int velM){
+	velocidadMaxima = velM;
+}
+
+int Movimiento::getVelMaxima(){
+	return velocidadMaxima;
+}
+
+void Movimiento::setAplicaMaxVel(bool aplMaxVel){
+	aplicaMaxVelocidad = aplMaxVel;
+}
+
+bool Movimiento::getAplicaMaxVel(){
+	return aplicaMaxVelocidad;
+}
+
+void Movimiento::VerAtributos(){
+	//imprimir atributos
+}
+
+/*
+int main(){
+	return 0;
+}*/
