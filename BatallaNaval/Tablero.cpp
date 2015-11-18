83045b693f1646b4173cce322caf38a22212137c
DeveloperVix <vicomisterio@hotmail.com>
2015-11-18 12:32:40 -0600

    Clase Tablero
    Seria la clase base la cual hereda jugador

---
+++ BatallaNaval/Tablero.cpp
@@ -0,0 +1,76 @@
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+class Tablero{
+public:
+	
+	void Mensajes(int);//Manda mensajes de lo que se ha hecho
+	void setBarcosPosicion();//Colocar los barcos en el tablero en base a coordenadas introducidas por el jugador
+	void VerificarMovimiento(int,int);//Introduce coordenadas y se verifica si se le dio a un barco
+	void setScore(int);
+	void getScore();
+	
+private:
+
+	int tablero[10][10];//tablero
+	int score;
+	
+};
+
+void Tablero::Mensajes(int opcion){
+	switch(opcion){
+	case 1:
+		cout<<"Has fallado!!"<<endl;
+		break;	
+	case 2:
+		cout<<"Barco undido!!"<<endl;
+		break;
+	case 3:
+		cout<<"Averiado!!"<<endl;
+		break;
+	case 4:
+		cout<<"Has ganado :D"<<endl;
+		break;
+	case 5:
+		cout<<"Has perdido DX"<<endl;
+		break;
+	case 6:
+		cout<<"Barco colocado"<<endl;
+		break;
+		
+	}
+}
+
+void Tablero::setBarcosPosicion(){
+	int tamanioBarco = 0, Xi = 0, Yi = 0, Xf = 0, Yf = 0;
+	cout<<"Tamanio del barco: "<<endl;
+	cin>>tamanioBarco;
+	
+	if(tamanio >= 2){
+		
+		cout<<"Coordenada inicial X:"<<endl;
+		cin>>Xi;
+		cout<<"Coordenada inicial Y:"<<endl;
+		cin>>Yi;
+		cout<<"Coordenada final X:"<<endl;
+		cin>>Xf;
+		cout<<"Coordenada final Y:"<<endl;
+		cin>>Yf;
+		
+		//definir el for en que direccion (coordenadas), debera asignar el barco
+		
+	}else{
+
+		cout<<"Coordenada X:"<<endl;
+		cin>>Xi;
+		cout<<"Coordenada Y:"<<endl;
+		cin>>Yi;
+		
+		tablero[Xi][Yi] = tamanioBarco;
+		Mensajes(6);
+	}
+	
+	//tablero[fila][columna] = 
+}
