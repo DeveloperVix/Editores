274a322f7f87997fd80e54c1763b6845bb47c4c3
DeveloperVix <vicomisterio@hotmail.com>
2015-11-22 14:56:39 -0600

    Metodo setBarcosPosicion
    Ya esta casi terminado, solo faltaría agregar que no se pueden encimar
    los barcos

--- BatallaNaval/Tablero.cpp
+++ BatallaNaval/Tablero.cpp
@@ -6,11 +6,13 @@ using namespace std;
 class Tablero{
 public:
 	
+	Tablero();
 	void Mensajes(int);//Manda mensajes de lo que se ha hecho
 	void setBarcosPosicion();//Colocar los barcos en el tablero en base a coordenadas introducidas por el jugador
 	void VerificarMovimiento(int,int);//Introduce coordenadas y se verifica si se le dio a un barco
 	void setScore(int);
-	void getScore();
+	int getScore();
+	
 	
 private:
 
@@ -18,6 +20,23 @@ private:
 	int score;
 	
 };
+
+Tablero::Tablero(){
+	int i,j;
+	for(i = 0; i < 10; i++){
+		for(j = 0; j < 10; j++){
+			tablero[i][j] = 0;
+		}
+	}
+}
+
+void Tablero::setScore(int nScore){
+	score = nScore;
+}
+
+int Tablero::getScore(){
+	return score;
+}
 
 void Tablero::Mensajes(int opcion){
 	switch(opcion){
@@ -39,16 +58,19 @@ void Tablero::Mensajes(int opcion){
 	case 6:
 		cout<<"Barco colocado"<<endl;
 		break;
+	case 7:
+		cout<<"Estan mal las coordenadas"<<endl;
+		break;
 		
 	}
 }
 
 void Tablero::setBarcosPosicion(){
-	int tamanioBarco = 0, Xi = 0, Yi = 0, Xf = 0, Yf = 0, auxP = 0;
+	int tamanioBarco = 0, Xi = 0, Yi = 0, Xf = 0, Yf = 0, auxP = 0, i,j;
 	cout<<"Tamanio del barco: "<<endl;
 	cin>>tamanioBarco;
 	
-	if(tamanio >= 2){
+	if(tamanioBarco >= 2){
 		
 		cout<<"Coordenada inicial X:"<<endl;
 		cin>>Xi;
@@ -58,40 +80,70 @@ void Tablero::setBarcosPosicion(){
 		cin>>Xf;
 		cout<<"Coordenada final Y:"<<endl;
 		cin>>Yf;
-		
-		//definir el for en que direccion (coordenadas), debera asignar el barco
+
 		
 		if(Xi == Xf){
 			
 			//se suma Y
 			if(Yi < Yf){
-				//for 
-			}else Yi > Yf{
+				
+				Xi--;
+				for(i = --Yi; i < Yf; i++){
+					tablero[Xi][i] = tamanioBarco;
+				}
+				Mensajes(6);
+				
+			}else if (Yi > Yf){
+				
 				auxP = Yf;
 				Yf = Yi;
 				Yi = auxP;
-				//for
+				Xi--;
+				for(i = --Yi; i < Yf; i++){
+					tablero[Xi][i] = tamanioBarco;
+				}
+				Mensajes(6);
+				
 			}else{
-				cout<<"Estan mal las coordenadas"
+				
+				Mensajes(7);
+				
 			}
 			
 		}else if(Yi == Yf){
 			
 			//se suma X
 			if(Xi < Xf){
-				//for 
-			}else Xi > Xf{
+				
+				Yi--;
+				for(i = --Xi; i < Xf; i++){
+					tablero[i][Yi] = tamanioBarco;
+				}
+				Mensajes(6);
+				
+			}else if (Xi > Xf){
+				
 				auxP = Xf;
 				Xf = Xi;
 				Xi = auxP;
-				//for
+				Yi--;
+				for(i = --Xi; i < Xf; i++){
+					tablero[i][Yi] = tamanioBarco;
+				}
+				Mensajes(6);
+				
 			}else{
-				cout<<"Estan mal las coordenadas"
+				Mensajes(7);
 			}
+			
+		}else{
+			
+			Mensajes(7);
+			
 		}
 		
 	}else{
-
+		//Barco de una casilla
 		cout<<"Coordenada X:"<<endl;
 		cin>>Xi;
 		cout<<"Coordenada Y:"<<endl;
@@ -101,5 +153,17 @@ void Tablero::setBarcosPosicion(){
 		Mensajes(6);
 	}
 	
-	//tablero[fila][columna] = 
+	
+	for(i = 0; i < 10; i++){
+		for(j = 0; j < 10; j++){
+			cout<<tablero[i][j]<<"";
+		}
+		cout<<"\n";
+	}
+}
+
+int main(){
+	Tablero tab;
+	tab.setBarcosPosicion();
+	return 0;
 }
