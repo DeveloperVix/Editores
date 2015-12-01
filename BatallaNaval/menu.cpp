3f2a9fd946a4f4dda4f632c3e95d5a5ac8f9df0c
DeveloperVix <vicomisterio@hotmail.com>
2015-11-30 23:13:52 -0600

    Hay que hacer cambios
    Recuerden que en esta clase se crea al objeto del tipo jugador, chequen
    los comentarios

--- BatallaNaval/menu.cpp
+++ BatallaNaval/menu.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
 #include "jugador.cpp"
-#include "movimiento.cpp"
-#include "tablero.cpp"
+
+#include "movimiento.cpp"//Este encabezado no va
+#include "tablero.cpp"	//por que en jugador.cpp hereda tanto la clase movimiento como tablero
 using namespace std;
 
 class menuBatalla{
 public:
 	
-	Batalla actorBatalla[2];
-	Tablero actorTablero[2];
-	Jugador actorJugador[2];
+	Batalla actorBatalla[2];//No va por que jugador puede acceder a los metodos
+	Tablero actorTablero[2];//No va por que jugador puede acceder a los metodos
 	
-	menuBatalla();
-	void menuInicial();
-	void jugadores();
-	void posicionaBarcos();
+	Jugador actorJugador[2];//Serian 3 jugadores ya que faltaria el juego contra la CPU 
+	
+	menuBatalla();//no se esta utilizando, pero aqui se podria llamar el metodo menuInicial			
+	void jugadores();	//Este metodo no iria
+	void iniciarBatalla(); //Este metodo no iria
+	
+	void posicionaBarcos();	//Este metodo iria en movimiento.cpp
+	void menuJuego();//Este metodo iria en movimiento.cpp
+	
 	int contador;
-	void menuJuego();
-	void iniciarBatalla();
+	void menuInicial();
+	
+	
 	
 private:
 	
@@ -40,7 +46,7 @@ void menuBatalla::posicionaBarcos(int contador){
 	actorTablero[contador].setBarcosPosicion();
 	
 }
-void menuBatalla::menuInicial(){
+void menuBatalla::menuInicial(){//hay que hacer cambios en las opciones, 1. Jugar contra la CPU, 2. Jugador VS Jugador (SOLO ABRIA QUE PONER UN AVISO DE QUE NO SE PUEDE POR QUE NO ESTA LISTA xD), 3. Salir
 	int n;
 	cout<<"Bienvenido al juego Batalla naval"<<endl;
 	cout<<"Menu"<<endl;
@@ -75,16 +81,16 @@ void menuBatalla::iniciarBatalla(int contador){
 	actorBatalla[contador].main();
 }
 
-
-void menuBatalla::menuJuego(){
+//Esto iria en movimiento.cpp
+void menuBatalla::menuJuego(){//Solo faltaria la parte de ver el tablero, no como una opcion, si no que salga antes del menu siempre
 	int n;
-	cout<<"¡Preparate para la batall!"<<endl;
+	cout<<"¡Preparate para la batalla!"<<endl;
 	cout<<"1. Atacar" <<endl;
 	cout<<"2. Rendirte"<<endl;
 	cin>> n;
 	
 	switch(n){
-	case 1:
+	case 1:				//En esta parte es donde hay que acceder a la clase de movimientos, lo cual hace el jugador pues hereda de movimiento
 		iniciarBatalla()
 		menuJuego();
 		break;
@@ -93,6 +99,8 @@ void menuBatalla::menuJuego(){
 		break;
 }
 
+
+//faltaria el main, ya que esta clase es la principal
 
 
 
